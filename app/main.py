849f24500eb88526972cb4f6fae39d0ef5c3159f
reis <joaoreis16@ua.pt>
2022-10-24 09:28:56 +0100

    init API and tests

--- app/main.py
+++ app/main.py
@@ -10,16 +10,40 @@ async def startup_event():
     app.state.redis = await init_redis_pool()
     # criar repositório bd
 
-@app.get("/hello")
-def hello():
+
+@app.get("/")
+def root():
     return {"message": "Hello world. O projeto está feito meus caros"}
 
 
-@app.get("/intrusion")
-def getIntrusionData():
-    return {"message": "get all data from cameras"}
+@app.post("/intrusion")
+def getIntrusionData(request):
+    data = request.data
+    print(data)
+    return {"message": "check if an intrusion occurs"}
 
 
-@app.put("/videoClips")
-def storeVideoClips():
-    return {"message": "send video clips to AWS S3"}
+@app.get("/intrusion/frames")
+def getVideoFrames():
+    return {"message": "get the video frames"}
+
+
+@app.get("/intrusion/video")
+def getVideo():
+    return {"message": "get video from cameras"}
+
+
+@app.post("/intrusion/video")
+def saveClips():
+    """ sendo to AWS S3 """
+    return {"message": "save the video clip in ASW S3"}
+
+
+@app.post("/intrusion/activate")
+def activateAlarms():
+    return {"message": "send to message queue to activate alarms"}
+
+
+@app.post("/intrusion/notification")
+def sendNotification():
+    return {"message": "send request to notification API"}
--- tests/__init__.py
+++
@@ -1,5 +0,0 @@
-""" from setuptools import setup, find_packages
-setup(
-    name = 'tests',
-    packages = find_packages()
-) """
