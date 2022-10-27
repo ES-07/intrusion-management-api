6b1bd27db70ec729d81a38432c3c6629a9bcebc7
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2022-10-27 16:17:26 +0100

    t

---
+++ app/tests/test_main.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+
+from api.main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/hello")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello world. O projeto está feito meus caros"}
