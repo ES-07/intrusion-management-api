05b387ccd9c9ef5cdb5cbdd10825538f8b6d1a23
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2022-10-27 16:28:43 +0100

    tests fixed + updated pipeline

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,18 @@
 from fastapi.testclient import TestClient
 
-from app.main import app
+from app.api.main import app
 
 client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get("/")
+    response = client.get("/hello")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world. O projeto está feito meus caros"}
 
 
 
 def test_getIntrusionData():
-    response = client.get("/intrusion")
+    response = client.get("/intrusion/frames")
     assert response.status_code == 200
-    assert response.json() == {"message": "check if an intrusion occurs"}
+    assert response.json() == {"message": "get the video frames"}
