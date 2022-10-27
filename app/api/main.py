12103be79434ffbee7879d775e5228a0682fba5a
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2022-10-27 17:42:49 +0100

    test

--- app/api/main.py
+++ app/api/main.py
@@ -4,6 +4,10 @@ from starlette.responses import Response
 app = FastAPI()
 # incluir router mais tarde3
 
+@app.get("/")
+def hello():    
+    return {"message": "CI/CD done in AWS EC2"}
+
 @app.get("/hello")
 def hello():    
     return {"message": "Hello world. O projeto está feito meus caros"}
